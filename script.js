e6272e79761b4e832a287a91389123dc20691680
SoloGudLyf <solomonteiko0@gmail.com>
2025-10-08 02:30:15 +0000

    Add styles,let users set grid size,randomize cell bgColor
    to position elements on the page making it look nice and also add
    progressive darkening effect upon hovering on cells

--- script.js
+++ script.js
@@ -1,9 +1,33 @@
 const container = document.querySelector(".container");
+const setGridSizeBtn = document.querySelector(".setGridSize");
+let gridSquareArr = [];
 
-function createGrid() {
-  for (let i = 0; i < 256; i++) {
+function createGrid(num) {
+  for (let i = 0; i < num ** 2; i++) {
     const div = document.createElement("div");
+    div.style.cssText = `width: calc(40vw/${num}); height:calc(40vw/${num})`;
     container.appendChild(div);
   }
 }
-createGrid();
+
+container.addEventListener("mouseover", (e) => {
+  let divHovered = e.target;
+  gridSquareArr.push(divHovered); // Help count number of squares hovered
+
+  //Set random color upon hovering a square
+  let randomColor = `rgb(${Math.floor(Math.random() * 256)} ${Math.floor(
+    Math.random() * 256
+  )} ${Math.floor(Math.random() * 256)}`;
+  divHovered.style.backgroundColor = randomColor;
+
+  // Produce progressive darkening effect 
+  divHovered.style.opacity = 1 - 0.1 * gridSquareArr.length;
+});
+
+setGridSizeBtn.addEventListener("click", () => {
+  container.innerHTML = "";
+  let size = +prompt("How many squares per side do you want for the new grid?");
+  createGrid(size);
+});
+
+createGrid(16);
